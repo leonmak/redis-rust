2d098e2ee7cf4723c14a72db95bbff4a839b7816
Leon Mak <leonmakansheng@gmail.com>
2024-07-09 00:56:55 +0800

    Init storage and use it in stream handler loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 
+use std::collections::HashMap;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
@@ -8,15 +9,19 @@ fn is_connection_open(stream: &TcpStream) -> bool {
     stream.peer_addr().is_ok()
 }
 
-fn handle_stream(stream: &mut TcpStream) -> Option<usize> {
+fn next_part(iter: &mut std::str::Split<&str>) -> String {
+    let _ = iter.next(); // ignore cmd_len, e.g. $4 for PING
+    iter.next().unwrap().to_owned()
+}
+
+fn handle_stream(stream: &mut TcpStream, map_store: &mut HashMap<String, String>) -> Option<usize> {
     // Note: redis-cli does not send EOF, so it is blocked on stream.read()
 
     // stream.read_to_string(&mut buffer)?;
-    // println!("buffer: {}", buffer);
 
-    // Read to vec then convert to string
+    // Buffered Read - copy to vec then convert to string
     let mut data = Vec::new();
-    let mut buf = [0u8; 10];
+    let mut buf = [0u8; 50];
     let mut bytes_read = 0;
     println!("Reading data...");
     loop {
@@ -35,6 +40,7 @@ fn handle_stream(stream: &mut TcpStream) -> Option<usize> {
         // println!("last two: {:?}", last_two);
     }
     println!("{:?} bytes read", bytes_read);
+    // println!("buffer: {:?}", String::from_utf8(Vec::from(buf)));
 
     // Split and process each line
     let buffer = String::from_utf8(data).unwrap();
@@ -42,26 +48,41 @@ fn handle_stream(stream: &mut TcpStream) -> Option<usize> {
     let mut iter = buffer.split("\r\n");
     // e.g. *12 = 12 commands
     let mut cmd_lines = iter.next().unwrap()[1..].parse::<i32>().unwrap();
-    cmd_lines -= 1;
-    println!("NUM_CMD_LINES, {}", cmd_lines);
-    let _ = iter.next(); // ignore cmd_len, e.g. $4 for PING
-    let cmd = iter.next().unwrap();
+    println!("NUM_LINES, {}", cmd_lines);
+
+    // Parse command
+    let cmd = next_part(&mut iter);
+    let cmd_str = cmd.as_str();
     let mut resp_s = String::new();
-    if cmd == "PING" {
+    println!("CMD: {}", cmd_str);
+
+    if cmd_str == "PING" {
         resp_s = "+PONG\r\n".to_owned();
     }
 
-    // parse args
+    // Parse args
+    cmd_lines -= 1;
     while cmd_lines > 0 {
         cmd_lines -= 1;
-        let _ = iter.next(); // ignore cmd_len, e.g. $4 for PING
-        let arg = iter.next().unwrap();
+        let arg = next_part(&mut iter);
         println!("ARG: {}", arg);
-        let resp_s_add = match cmd {
-            "ECHO" => format!("+{}\r\n", arg),
-            _ => "+OK\r\n".to_owned(),
+        let resp_s_arg = match cmd_str {
+            "ECHO" => arg,
+            "GET" => {
+                let val = map_store.get(&arg).expect("NO VALUE");
+                val.to_owned()
+            }
+            "SET" => {
+                let val = next_part(&mut iter);
+                println!("VAL: {}", val);
+                cmd_lines -= 1;
+                map_store.insert(arg, val);
+                "OK".to_owned()
+            }
+            _ => "OK".to_owned(),
         };
-        resp_s.push_str(&resp_s_add);
+        let res_str = format!("+{}\r\n", resp_s_arg);
+        resp_s.push_str(&res_str);
     }
     println!(">> {}", resp_s);
     bytes_written += stream.write(resp_s.as_bytes()).unwrap();
@@ -78,8 +99,9 @@ fn main() {
         if let Ok(mut s) = stream {
             println!("Accepted new connection");
             thread::spawn(move || {
+                let mut map: HashMap<String, String> = HashMap::new();
                 while is_connection_open(&s) {
-                    if handle_stream(&mut s) == None {
+                    if handle_stream(&mut s, &mut map) == None {
                         break;
                     };
                 }
